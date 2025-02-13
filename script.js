eb44d17786cf2e17aee7351162afe759cb0a7c50
Diego Gelvez Esparza <digel95@gmail.com>
2025-02-13 14:47:43 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Función para abrir el modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+// Función para cerrar el modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
